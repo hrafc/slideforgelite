5bd0db7f2f1301cbbeb564e622dced811557253f
hrafc <otagillug@gmail.com>
2026-03-25 19:53:11 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = "slideforge-lite-v1";
+const ASSETS = [
+  "/slideforgelite/",
+  "/slideforgelite/index.html",
+  "/slideforgelite/manifest.json",
+  "/slideforgelite/logo-star.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => response || fetch(event.request))
+  );
+});
