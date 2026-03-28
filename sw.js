8bb24f6c2dcab7dcfb9fd70418ac8d6cf389eb0c
hrafc <otagillug@gmail.com>
2026-03-28 10:05:11 +0100

    Update service worker cache name and add activation event

--- sw.js
+++ sw.js
@@ -1,8 +1,10 @@
-const CACHE_NAME = "slideforge-lite-v10.1.1";
+const CACHE_NAME = "slideforge-lite-v10.2.0";
 const ASSETS = [
   "/slideforgelite/",
   "/slideforgelite/index.html",
   "/slideforgelite/manifest.json",
+  "/slideforgelite/icon-192.png",
+  "/slideforgelite/icon-512.png",
   "/slideforgelite/logo-star.png"
 ];
 
@@ -10,6 +12,20 @@ self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys
+          .filter((key) => key !== CACHE_NAME)
+          .map((key) => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", (event) => {
